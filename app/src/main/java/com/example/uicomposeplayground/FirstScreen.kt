e6378f5e8afdbd67d1eee4da371a4a5b5ccded23
Vlad <ricean.vlad7@gmail.com>
2023-03-05 15:47:35 +0200

    Empty composable

---
+++ app/src/main/java/com/example/uicomposeplayground/FirstScreen.kt
@@ -0,0 +1,14 @@
+package com.example.uicomposeplayground
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FirstScreen(modifier: Modifier = Modifier) {
+
+
+}
